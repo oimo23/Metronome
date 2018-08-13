f2d27d6beb732dbb468cd149b17f3642b3171663
oimo23 <oimo2338@gmail.com>
2018-08-13 14:50:15 +0900

    WebAudioスマホ対応実験

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -12,19 +12,6 @@ let eventType = (isTouchDevice) ? 'touchend' : 'click';
 
 let first_click = true;
 
-/*-----------------------------------------------
-// 低い音の準備
--------------------------------------------------*/
-const context = new(window.AudioContext||window.webkitAudioContext)();
-const osc = context.createOscillator();
-const gain = context.createGain();
-
-/*-----------------------------------------------
-// 高い音の準備
--------------------------------------------------*/
-const context2 = new AudioContext();
-const osc2 = context2.createOscillator();
-const gain2 = context2.createGain();
 
 /*-----------------------------------------------
 // 再生処理 
