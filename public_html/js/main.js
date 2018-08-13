0eb76ae2ebc2d7741093ee9ad71b39bdc4dc2c89
oimo23 <oimo2338@gmail.com>
2018-08-13 14:53:11 +0900

    WebAudioスマホ対応実験

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,15 +1,15 @@
 /*-----------------------------------------------
 // 低い音の準備
 -------------------------------------------------*/
-const context = new(window.AudioContext||window.webkitAudioContext)();
-const osc = context.createOscillator();
-const gain = context.createGain();
+var context = new(window.AudioContext||window.webkitAudioContext)();
+var osc = context.createOscillator();
+var gain = context.createGain();
 
 /*-----------------------------------------------
 // 高い音の準備
 -------------------------------------------------*/
-const context2 = new AudioContext();
-const osc2 = context2.createOscillator();
-const gain2 = context2.createGain();
+var context2 = new(window.AudioContext||window.webkitAudioContext)();
+var osc2 = context2.createOscillator();
+var gain2 = context2.createGain();
 
 alert('hello');
